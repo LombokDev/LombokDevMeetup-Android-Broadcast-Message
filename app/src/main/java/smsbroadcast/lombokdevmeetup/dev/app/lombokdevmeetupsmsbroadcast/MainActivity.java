ed3b3f0d6b21b69fa60e3a1623ecfa766faefc54
Khairul Imam <ki65559@gmail.com>
2017-10-09 00:04:39 +0800

    edit indentation

--- app/src/main/java/smsbroadcast/lombokdevmeetup/dev/app/lombokdevmeetupsmsbroadcast/MainActivity.java
+++ app/src/main/java/smsbroadcast/lombokdevmeetup/dev/app/lombokdevmeetupsmsbroadcast/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -78,8 +79,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 					InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
 					CSVReader csvReader = new CSVReader(inputStreamReader, ',');
 					List<String[]> forms = csvReader.readAll();
-					List<String[]> skipedFirstRow = forms.subList(1, forms.size());
-					Stream.of(skipedFirstRow).forEach(s -> etMessage.setText(etMessage.getText().toString() + s[1] + "\n"));
+					List<String[]> skipFirstRow = forms.subList(1, forms.size());
+					Stream.of(skipFirstRow).forEach(s -> Log.d("csv content", TextUtils.join(" ", s)));
 				} catch (FileNotFoundException e) {
 					e.printStackTrace();
 				} catch (IOException e) {
